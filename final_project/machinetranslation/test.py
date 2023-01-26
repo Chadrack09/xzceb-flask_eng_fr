b7fc6c98196b6aa7e92957829feb7d843961cf9d
Chaadrack <bbenolych@gmail.com>
2023-01-26 15:36:50 -0500

    made changes to file

---
+++ final_project/machinetranslation/test.py
@@ -0,0 +1,18 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestEnglishTranslator(unittest.TestCase):
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french(""),"Please enter a valid English word")
+        self.assertEqual(english_to_french("Hello"),"Bonjour")
+        self.assertEqual(english_to_french("Goodbye"),"Au revoir")
+        self.assertNotEqual(english_to_french("Goodbye"),"Bonjour")
+
+class TestFrenchTranslator(unittest.TestCase):
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english(""),"Entrez un mot français valide")
+        self.assertEqual(french_to_english("Bonjour"),"Hello")
+        self.assertEqual(french_to_english("Au revoir"),"Goodbye")
+        self.assertNotEqual(french_to_english("Bonjour"),"Goodbye")
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,10 +9,22 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-def englishToFrench(englishText):
-    #write the code here
-    return frenchText
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+language_translator.set_service_url(url)
 
-def frenchToEnglish(frenchText):
-    #write the code here
-    return englishText
+def english_to_french(english_text):
+    if (len(english_text) > 0):
+        """ Translating the input from English to French"""
+        translation = language_translator.translate(english_text, model_id='en-fr').get_result()
+        return translation['translations'][0]['translation']
+    else:
+        return "Please enter a valid English word"
+
+def french_to_english(french_text):
+    if(len(french_text) >0):
+        """ Translating the input from French to English"""
+        translation = language_translator.translate(french_text, model_id='fr-en').get_result()
+        return translation['translations'][0]['translation']
+    else:
+        return "Entrez un mot français valide"
